651ef02d6fdb2d3bba73985abe0679dbd272b88a
CHN-STUDENT <CHN-STUDENT@outlook.com>
2018-07-03 10:47:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="srun-cli",
-    version="0.1.0",
+    version="0.1.1",
     license='http://www.apache.org/licenses/LICENSE-2.0',
     description="A Command line interface for Srun3k Client for HAUT",
     author='CHN-STUDENT',
